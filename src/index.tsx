1e936afb27829d475b828cebafbaa3afd50563f2
Michalis Mavromatakis <m.mavromatakis@flexcar.gr>
2023-07-19 17:42:14 +0300

    Update ReactDOM rendering

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import Routing from "./Routing";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+
+ReactDOM.createRoot(rootElement as HTMLElement).render(
   <BrowserRouter>
     <React.Fragment>
       <Routing />
       {/* The various pages will be displayed by the `Main` component. */}
     </React.Fragment>
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
